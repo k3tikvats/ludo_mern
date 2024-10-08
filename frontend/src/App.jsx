58afc6403a28bd2c6238d896b9da8771e8671efc
k3tikvats <kartikvats_23cs214@dtu.ac.in>
2024-10-09 01:39:35 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import LandingPage from './Components/landingpage/lpg'; // Make s
-
-
 export default function App() {
   return <h1 className="text-3xl font-bold underline">Basic Template</h1>;
 }
